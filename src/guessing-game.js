04b8ce981073fd0a58eafcb2d5526faa63edd54e
rebel228 <dinovik@rambler.ru>
2023-02-25 22:23:10 +0300

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
+let l = 0;
+let r = 0;
+let n = 0;
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        l = min;
+        r = max;
     }
 
     guess() {
-
+        n = Math.round((l + r) / 2);
+        return n;
     }
 
     lower() {
-
+        r = n;
     }
 
     greater() {
-
+        l = n;
     }
 }
 
